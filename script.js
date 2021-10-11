dfe91c0ac2c3c7abb133d7b0cfd495ceb0ee5907
sachin kumar <merugune.sachin123@gmail.com>
2021-10-11 22:11:00 +0530

    strings in javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+var myString = 'javascript'
+console.log(myString)
+console.log(typeof(myString))
+
+var secondString = 'javascript is light weight cross platform'
+console.log(typeof(secondString))
+
+var doubleQuoteString = "javascript is \"light weight\" cross platform"
+console.log((doubleQuoteString))
+
+var singleQuoteString = 'javascript is \'light weight\' \'cross platform\''
+console.log((singleQuoteString))
+
+var doubleQuoteString2 = "javascript is 'light weight' cross platform"
+console.log((doubleQuoteString2))
+
+var singleQuoteString2 = 'javascript is "light weight" "cross platform"'
+console.log((singleQuoteString2))
